20e250cfbd637e4056f234830b04a176c6a02031
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2018-04-15 12:06:58 +0300

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def check_pin?(pin)
-    return if ENV['RPI'] == '0' && pin.is_a?(String)
+    return if ENV['RPI'] == '0' || pin.is_a?(String)
     RPi::GPIO.setup pin, as: :input
     RPi::GPIO.high?(pin)
   end
